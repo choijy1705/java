f005a05268e9cb6f6bcdd3133642df6cde5aa541
choijy1705 <63634505+choijy1705@users.noreply.github.com>
2020-06-03 18:14:01 +0900

    a

--- practice/src/SWexpert/Swea5431.java
+++ practice/src/SWexpert/Swea5431.java
@@ -1,9 +1,34 @@
 package SWexpert;
-
+import java.util.*;
 public class Swea5431 {
-
+	static Scanner sc = new Scanner(System.in);
+	static int C;
+	static int N;
+	static int K;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		C = sc.nextInt();
+		
+		for(int tc=1;tc<=C;tc++) {
+			N = sc.nextInt();
+			K = sc.nextInt();
+			List<Integer> list = new ArrayList<>();
+			for(int i=1;i<=N;i++) {
+				list.add(i);
+			}
+			
+			for(int i=0;i<K;i++) {
+				list.remove((Integer)sc.nextInt());
+			}
+			
+			String result = "";
+			for(int i=0;i<list.size();i++) {
+				result += list.get(i) + " ";
+			}
+			
+			System.out.println("#" + tc + " " + result);
+		}
+			
 
 	}
 
