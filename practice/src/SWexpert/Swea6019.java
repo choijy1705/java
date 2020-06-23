993297e9190e8c4fa96efa8f5fd7e5a93ef57bc7
choijy1705 <63634505+choijy1705@users.noreply.github.com>
2020-06-23 17:59:42 +0900

    a

--- practice/src/SWexpert/Swea6019.java
+++ practice/src/SWexpert/Swea6019.java
@@ -4,29 +4,25 @@ import java.util.*;
 public class Swea6019 {
 	static Scanner sc = new Scanner(System.in);
 	static int C;
-	static int A;
-	static int B;
-	static int fly;
-	static int distance;
+	static double A;
+	static double B;
+	static double fly;
+	static double distance;
 	public static void main(String[] args) {
 		C = sc.nextInt();
 		
 		for(int tc=1;tc<=C;tc++) {
-			distance = sc.nextInt();
-			A = sc.nextInt();
-			B = sc.nextInt();
-			fly = sc.nextInt();
-			
-			int dir = 0;
-			double realtime = 0;
+			distance = sc.nextDouble();
+			A = sc.nextDouble();
+			B = sc.nextDouble();
+			fly = sc.nextDouble();
+		
 			double crushtime = distance/(A+B);
 			
-
 			double flydistance = crushtime * fly;
+	
 			
-			flydistance = Math.round(flydistance*1000000)/1000000;
-			
-			System.out.printf("#%d %.6f",tc,flydistance);
+			System.out.printf("#%d %.10f",tc,flydistance);
 		}
 
 	}
